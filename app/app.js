47a2c2d342e18e845aef6231e32ae64634cde2fc
mgarbacz <michal@garbacz.io>
2019-05-25 16:41:38 -0700

    Create p5 canvas

---
+++ app/app.js
@@ -0,0 +1,4 @@
+import sketch from './sketch.js'
+
+const container = new p5(sketch, 'app')
+console.log(container)
---
+++ app/sketch.js
@@ -0,0 +1,13 @@
+export default function(p5) {
+  // p5 init
+  p5.setup = function() {
+    p5.createCanvas(800, 800)
+    p5.noStroke()
+    p5.background('#333')
+  }
+
+  // p5 draw loop
+  p5.draw = function() {
+
+  }
+}
