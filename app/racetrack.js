cf65262c886c763714335b457a61de8a892ff412
mgarbacz <michal@garbacz.io>
2019-05-25 22:16:03 -0700

    Modify racetrack and add start/finish line

--- app/racetrack.js
+++ app/racetrack.js
@@ -4,13 +4,21 @@ export default class Racetrack {
   }
 
   display() {
+    // track settings
     this.p5.noFill()
     this.p5.stroke('#333')
     this.p5.strokeWeight(80)
-    this.p5.curve(600, 600, 300, 100, 900, 100, 400, 400)
-    this.p5.curve(0, 0, 900, 100, 400, 300, 0, 800)
-    this.p5.bezier(400, 300, 300, 500, 1000, 400, 1100, 700)
-    this.p5.line(1100, 700, 200, 700)
-    this.p5.curve(800, 800, 200, 700, 300, 100, 800, 0)
+
+    // draw track lines
+    this.p5.line(200, 100, 1100, 100)
+    this.p5.bezier(400, 500, 300, 300, 1000, 400, 1100, 100)
+    this.p5.curve(600, 200, 300, 700, 900, 700, 400, 400)
+    this.p5.curve(0, 800, 900, 700, 400, 500, 0, 0)
+    this.p5.curve(800, 0, 200, 100, 300, 700, 800, 800)
+
+    // draw start/finish line
+    this.p5.stroke('#fafafa')
+    this.p5.strokeWeight(10)
+    this.p5.line(600, 50, 600, 150)
   }
 }
