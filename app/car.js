3ca427eecae92c4f8c827d0b7e18bc802eb23a05
mgarbacz <michal@garbacz.io>
2019-05-25 17:45:17 -0700

    Add beginnings of movement interaction

--- app/car.js
+++ app/car.js
@@ -3,11 +3,50 @@ export default class Car {
     this.x = x
     this.y = y
     this.sprite = sprite
+    this.width = sprite.width
+    this.height = sprite.height
+    this.accel = p5.createVector(0, 0)
+
     this.p5 = p5
   }
 
   // called every draw loop
   display() {
+    this.x += this.accel.x
+    this.y += this.accel.y
     this.p5.image(this.sprite, this.x, this.y)
+  }
+
+  // if wasd keys pressed, do some moves
+  handleKeyPress(key) {
+    if (key === 'w') {
+      this.move(-1)
+    } else if (key === 'a') {
+      this.turn(-1)
+    } else if (key === 's') {
+      this.move(1)
+    } else if (key === 'd') {
+      this.turn(1)
+    }
+
+  }
+
+  // if wasd keys released, stop the moves
+  handleKeyRelease(key) {
+    if (key === 'w' || key === 's') {
+      this.move(0)
+    } else if (key === 'a' || key === 'd') {
+      this.turn(0)
+    }
+  }
+
+  // called when movement key pressed or released
+  move(direction) {
+    this.accel.y = direction
+  }
+
+  // called when turn key pressed or released
+  turn(direction) {
+    this.accel.x = direction
   }
 }
--- app/sketch.js
+++ app/sketch.js
@@ -12,13 +12,23 @@ export default function(p5) {
   p5.setup = function() {
     p5.createCanvas(800, 800)
     p5.noStroke()
-    p5.background('#333')
 
     car = new Car(p5.width / 2, p5.height / 2, carSprite, p5)
   }
 
   // p5 draw loop
   p5.draw = function() {
+    p5.background('#333')
     car.display()
+  }
+
+  // called on every key press
+  p5.keyPressed = function() {
+    car.handleKeyPress(p5.key)
+  }
+
+  // called on every key release
+  p5.keyReleased = function() {
+    car.handleKeyRelease(p5.key)
   }
 }
