c95a8cbe03a73e44e13d23af350f9ca7d20217d2
mgarbacz <michal@garbacz.io>
2019-05-25 19:30:47 -0700

    Refactor car display into separate functions

--- app/car.js
+++ app/car.js
@@ -16,78 +16,75 @@ export default class Car {
 
   // called every draw loop
   display() {
-    // handle forward and backward movement keys
+    this._handleMovementKeys()
+    this._handleTurnKeys()
+    this._calculateNewAngle()
+    this._calculateNewPosition()
+    this._translate()
+    this._drawCar()
+  }
+
+  // handle forward and backward movement keys
+  _handleMovementKeys() {
     if (this.p5.keyIsDown(W_KEY) && this.p5.keyIsDown(S_KEY)) {
-      this.move(0)
+      this._move(0)
     } else if (this.p5.keyIsDown(W_KEY)) {
-      this.move(-1)
+      this._move(-1)
     } else if (this.p5.keyIsDown(S_KEY)) {
-      this.move(1)
+      this._move(1)
     } else {
-      this.move(0)
+      this._move(0)
     }
+  }
 
-    // handle left and right turn keys
+  // handle left and right turn keys
+  _handleTurnKeys() {
     if (this.p5.keyIsDown(A_KEY) && this.p5.keyIsDown(D_KEY)) {
-      this.turn(0)
+      this._turn(0)
     } else if (this.p5.keyIsDown(A_KEY)) {
-      this.turn(-1)
+      this._turn(-1)
     } else if (this.p5.keyIsDown(D_KEY)) {
-      this.turn(1)
+      this._turn(1)
     } else {
-      this.turn(0)
-    }
-
-    // calculate new angle, we only turn if moving
-    if (this.accel) {
-      this.angle += this.rotation
-    }
-    // calcuate new position based on accel
-    this.x += -this.accel * this.p5.sin(this.angle)
-    this.y += this.accel * this.p5.cos(this.angle)
-
-    // translate to new position
-    this.p5.translate(this.x, this.y)
-    // calculate the rotation
-    this.p5.rotate(this.angle)
-    // adjust car to center of its location based on sprite size
-    // this is done after the rotation so that rotation is centered on car
-    this.p5.translate(-this.width / 2, -this.height / 2)
-
-    // actually draw the car sprite
-    this.p5.image(this.sprite, 0, 0)
-  }
-
-  // if wasd keys pressed, do some moves
-  handleKeyPress(key) {
-    if (key === 'w') {
-      this.move(-1)
-    } else if (key === 'a') {
-      this.turn(-1)
-    } else if (key === 's') {
-      this.move(1)
-    } else if (key === 'd') {
-      this.turn(1)
-    }
-
-  }
-
-  // if wasd keys released, stop the moves
-  handleKeyRelease(key) {
-    if (key === 'w' || key === 's') {
-      this.move(0)
-    } else if (key === 'a' || key === 'd') {
-      this.turn(0)
+      this._turn(0)
     }
   }
 
   // called when movement key pressed or released
-  move(direction) {
+  _move(direction) {
     this.accel = direction
   }
 
   // called when turn key pressed or released
-  turn(direction) {
+  _turn(direction) {
     this.rotation = direction
+  }
+
+  // calculate new angle, we only turn if moving
+  _calculateNewAngle() {
+    if (this.accel) {
+      this.angle += this.rotation
+    }
+  }
+
+  // calcuate new position based on accel
+  _calculateNewPosition() {
+    this.x += -this.accel * this.p5.sin(this.angle)
+    this.y += this.accel * this.p5.cos(this.angle)
+  }
+
+  // translate to new position and rotation
+  _translate() {
+    this.p5.translate(this.x, this.y)
+    this.p5.rotate(this.angle)
+
+    // adjust car to center of its location based on sprite size
+    // this is done after the rotation so that rotation is centered on car
+    this.p5.translate(-this.width / 2, -this.height / 2)
+  }
+
+  // actually draw the car sprite
+  _drawCar() {
+    this.p5.image(this.sprite, 0, 0)
   }
 }
