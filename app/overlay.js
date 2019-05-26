de4665f12e2b06b2975ec95080228098eef4a928
mgarbacz <michal@garbacz.io>
2019-05-26 13:18:43 -0700

    Move overlay to an empty part of canvas

--- app/overlay.js
+++ app/overlay.js
@@ -1,14 +1,16 @@
 export default class Overlay {
   constructor(car, p5) {
+    this.x = 950
+    this.y = 400
     this.car = car
     this.p5 = p5
   }
 
   display() {
     this.p5.textSize(16)
-    this.p5.text(`Mouse X: ${this.p5.mouseX}`, 10, 20)
-    this.p5.text(`Mouse Y: ${this.p5.mouseY}`, 10, 40)
-    this.p5.text(`Car X: ${this.car.x}`, 10, 60)
-    this.p5.text(`Car Y: ${this.car.y}`, 10, 80)
+    this.p5.text(`Mouse X: ${this.p5.mouseX}`, this.x, this.y)
+    this.p5.text(`Mouse Y: ${this.p5.mouseY}`, this.x, this.y + 20)
+    this.p5.text(`Car X: ${this.car.x}`, this.x, this.y + 40)
+    this.p5.text(`Car Y: ${this.car.y}`, this.x, this.y + 60)
   }
 }
